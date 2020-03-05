d476fed573cedcebd0e86a709dbf18ec7016b608
Maaack <marek.belski@gmail.com>
2020-03-04 21:42:00 -0800

    GAME-01: Clean up and organize directories

--- objects/CenteredHUD.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-onready var arrow = $Arrow
-var arrow_rotation = 0 setget set_arrow_rotation
-
-func set_arrow_rotation(value):
-	arrow_rotation = value
-	rotation = arrow_rotation
