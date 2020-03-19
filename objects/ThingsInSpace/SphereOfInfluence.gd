a596674b33d819fb21a0f6cabe93e91e274d5ffc
Maaack <marek.belski@gmail.com>
2020-03-19 11:48:45 -0700

    GAME-05: Example of ring compositions orbiting at various ranges.

--- objects/ThingsInSpace/SphereOfInfluence.gd
+++ objects/ThingsInSpace/SphereOfInfluence.gd
@@ -8,9 +8,8 @@ onready var gravity_space_collider = $Area2D/CollisionShape2D
 onready var planet = $Planet8
 onready var planet_collider = $Planet8/CollisionShape2D
 
+export var sphere_size_mod = 1.5
 export var asteroid_spawn_distance_from_surface = 15000.0
-export var asteroid_spawn_distance_from_edge = 15000.0
-export var orbiting_asteroid_max = 1000
 enum OrbitDirectionSetting{CLOCKWISE, COUNTER_CLOCKWISE, EITHER}
 export(OrbitDirectionSetting) var orbit_direction
 export var vector_scale_mod = 1.0
@@ -19,8 +18,39 @@ var orbiting_node_scene = preload("res://objects/ThingsInSpace/OrbitingNode2D.ts
 var asteroid_scene = preload("res://objects/ThingsInSpace/Asteroid.tscn")
 var asteroid_texture = preload("res://assets/kenney_spaceshooterextension/PNG/Sprites X2/Meteors/spaceMeteors_004.png")
 
+class AsteroidRing:
+	var color
+	var count
+	var ring_inner_edge
+	var ring_outer_edge
+	
+	func _init(init_color:Color, init_count:int, init_ring_inner_edge:float, init_ring_outer_edge:float):
+		color = init_color
+		count = init_count
+		ring_inner_edge = init_ring_inner_edge
+		ring_outer_edge = init_ring_outer_edge
+
+var colors = [
+	Color( 0.207843, 0.309804, 0.866667, 1 ),
+	Color( 0.67451, 0.243137, 0.831373, 1 ),
+	Color( 0.839216, 0.282353, 0.231373, 1 ),
+	Color( 0.843137, 0.513726, 0.2, 1 ),
+	Color( 0.831373, 0.835294, 0.180392, 1 ),
+	Color( 0.219608, 0.792157, 0.168627, 1 )
+]
+
+var asteroid_belts = [
+	AsteroidRing.new(colors[0], 1000, 16000.0, 64000.0),
+	AsteroidRing.new(colors[1], 200, 32000.0, 48000.0),
+	AsteroidRing.new(colors[2], 200, 16000.0, 32000.0),
+	AsteroidRing.new(colors[3], 200, 8000.0, 16000.0),
+	AsteroidRing.new(colors[4], 200, 6000.0, 12000.0),
+	AsteroidRing.new(colors[5], 200, 0.0, 8000.0)
+]
+
 export(NodePath) var player_character_path
 var player_character
+
 
 func get_space_radius():
 	return gravity_space_collider.shape.radius
@@ -28,20 +58,12 @@ func get_space_radius():
 func get_planet_radius():
 	return planet_collider.shape.radius
 
-func get_random_orbit_radius():
-	var min_spawn_radius = get_planet_radius() + asteroid_spawn_distance_from_surface
-	var max_spawn_radius = get_space_radius() - asteroid_spawn_distance_from_edge
-	return rand_range(min_spawn_radius, max_spawn_radius)
+func get_min_spawn_radius():
+	return get_planet_radius() + asteroid_spawn_distance_from_surface
 
-func get_random_orbit_position():
+func get_random_vector():
 	var random_rotation = rand_range(0, PI*2)
-	var direction = Vector2(1,0).rotated(random_rotation)
-	var random_radius = get_random_orbit_radius()
-
-	return direction * random_radius
-
-func get_random_orbit_position_in_world_space():
-	return get_position_in_world_space() + get_random_orbit_position()
+	return Vector2(1,0).rotated(random_rotation)
 
 func get_orbital_velocity(start_position:Vector2):
 	if orbit_direction == OrbitDirectionSetting.EITHER:
@@ -71,11 +93,22 @@ func spawn_orbiting_sprite(object_scene:PackedScene, object_sprite:Sprite, objec
 	instance.orbit_object_texture_scale = object_sprite.scale
 	instance.player_character = player_character
 	add_child(instance)
+	return instance
+
+func spawn_asteroid_belts():
+	var min_spawn_radius = get_min_spawn_radius()
+	var asteroid_instance = asteroid_scene.instance()
+	var sprite = asteroid_instance.get_node("Sprite")
+	for asteroid_belt in asteroid_belts:
+		for _i in range(asteroid_belt.count):
+			var random_orbit_radius = rand_range(asteroid_belt.ring_inner_edge, asteroid_belt.ring_outer_edge)
+			random_orbit_radius += min_spawn_radius
+			var random_position = get_random_vector() * random_orbit_radius
+			var instance = spawn_orbiting_sprite(asteroid_scene, sprite, random_position)
+			instance.sprite.modulate = asteroid_belt.color
 
 func _ready():
 	if player_character_path != null:
 		player_character = get_node(player_character_path)
-	var asteroid_instance = asteroid_scene.instance()
-	var sprite = asteroid_instance.get_node("Sprite")
-	for i in range(orbiting_asteroid_max):
-		spawn_orbiting_sprite(asteroid_scene, sprite, get_random_orbit_position())
+	spawn_asteroid_belts()
+	gravity_space_collider.shape.radius *= sphere_size_mod
