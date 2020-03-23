fb7ccfa4215051cee6f4ad366f5fed6220da2166
Maaack <marek.belski@gmail.com>
2020-03-23 10:12:09 -0700

    UNIT-01: Start on new physical units and quantities.

---
+++ Resources/New/PhysicalQuantity.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+
+class_name PhysicalQuantity
+
+export(Resource) var physical_unit
+
+export(float) var quantity
---
+++ Resources/New/PhysicalUnit.gd
@@ -0,0 +1,12 @@
+extends Resource
+
+
+class_name PhysicalUnit
+
+export(String) var group_name
+export(String) var readable_name
+
+export(Vector2) var size
+export(float) var mass
+enum NumericalUnitSetting{ DISCRETE, CONTINUOUS }
+export(NumericalUnitSetting) var numerical_unit
