d2e921a5595330d8f0a55e7591cf0a4c6377ce6e
Maaack <marek.belski@gmail.com>
2020-03-27 11:18:35 -0700

    GAME-06: Organizing simple rockets under commandable ships.

--- Objects/WorldSpace/Ownable/Commandable/Ships/BaseCombatShip.gd
+++ Objects/WorldSpace/Ownable/Commandable/Ships/BaseCombatShip.gd
@@ -1,12 +1,9 @@
 extends "res://Objects/WorldSpace/Ownable/Commandable/CombatShip.gd"
 
 
-onready var ship_sprite = $Sprite
-
 func _on_CollectableArea_body_entered(body):
 	var empty_space = contents.get_empty_space()
 	if empty_space > 0:
 		if body.has_method('collect'):
 			var quantity = body.collect()
 			add_quantity_to_contents(quantity)
-
