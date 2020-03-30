909d653aa2519a2c7a31c5755591b3a6c884e5ca
Maaack <marek.belski@gmail.com>
2020-03-29 21:23:44 -0700

    GAME-08: Adjust background color of rocket in inspector.

--- Objects/Interface/Window/RocketInspector/RocketInspector.gd
+++ Objects/Interface/Window/RocketInspector/RocketInspector.gd
@@ -2,8 +2,9 @@ extends Control
 
 
 onready var texture_node = $MarginContainer/NinePatchRect/MarginContainer/TextureRect
-onready var centered_node = $MarginContainer/NinePatchRect/MarginContainer/TextureRect/CenterContainer/CenteredOverlay
+onready var centered_node = $MarginContainer/NinePatchRect/MarginContainer/CenterContainer/CenteredOverlay
 
+export(Color) var ship_color = Color(0.5, 0.5, 0.5)
 var icon_scene = preload("res://Objects/Interface/Icon/FreePositionIcon.tscn")
 
 func _input(event):
@@ -15,6 +16,7 @@ func _input(event):
 
 func show_ship(ship_node:Node2D):
 	texture_node.texture = ship_node.sprite.texture
+	texture_node.modulate = ship_color
 	var ship_texture_size = ship_node.sprite.texture.get_size()
 	var maximum_size = texture_node.rect_size
 	var scale_ratio_vector = maximum_size / ship_texture_size
@@ -25,6 +27,5 @@ func show_ship_mounts(ship_node:Node2D, scale_ratio:float):
 	for mount in ship_node.get_children():
 		if mount.is_in_group('MOUNT'):
 			var icon_instance = icon_scene.instance()
-			print("mount.position", str(mount.position))
 			centered_node.add_child(icon_instance)
 			icon_instance.position = mount.position * scale_ratio
