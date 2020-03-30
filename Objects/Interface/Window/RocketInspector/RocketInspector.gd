ec9c7ec053cd167a6a77d1e50e0c5b48cd5dab89
Maaack <marek.belski@gmail.com>
2020-03-29 21:31:21 -0700

    GAME-08: Adjust background color a better way. Using better buttons.

--- Objects/Interface/Window/RocketInspector/RocketInspector.gd
+++ Objects/Interface/Window/RocketInspector/RocketInspector.gd
@@ -4,8 +4,8 @@ extends Control
 onready var texture_node = $MarginContainer/NinePatchRect/MarginContainer/TextureRect
 onready var centered_node = $MarginContainer/NinePatchRect/MarginContainer/CenterContainer/CenteredOverlay
 
-export(Color) var ship_color = Color(0.5, 0.5, 0.5)
 var icon_scene = preload("res://Objects/Interface/Icon/FreePositionIcon.tscn")
+var info_dialog_scene = preload("res://Objects/Interface/Window/RocketInspector/InfoDialog/InfoDialog.tscn")
 
 func _input(event):
 	if event.is_action_pressed("ui_inspect"):
@@ -16,7 +16,6 @@ func _input(event):
 
 func show_ship(ship_node:Node2D):
 	texture_node.texture = ship_node.sprite.texture
-	texture_node.modulate = ship_color
 	var ship_texture_size = ship_node.sprite.texture.get_size()
 	var maximum_size = texture_node.rect_size
 	var scale_ratio_vector = maximum_size / ship_texture_size
@@ -26,6 +25,6 @@ func show_ship(ship_node:Node2D):
 func show_ship_mounts(ship_node:Node2D, scale_ratio:float):
 	for mount in ship_node.get_children():
 		if mount.is_in_group('MOUNT'):
-			var icon_instance = icon_scene.instance()
+			var icon_instance = info_dialog_scene.instance()
 			centered_node.add_child(icon_instance)
 			icon_instance.position = mount.position * scale_ratio
