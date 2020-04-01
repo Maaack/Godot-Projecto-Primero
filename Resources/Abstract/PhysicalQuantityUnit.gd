75e5084912a431a5522bd00f97c0a34bae6615c5
Maaack <marek.belski@gmail.com>
2020-04-01 10:54:06 -0700

    RES-01: Begin refactoring resources for unifying quantities into units. Creating a new quantity for every new unit got old fast.

---
+++ Resources/Abstract/PhysicalQuantityUnit.gd
@@ -0,0 +1,62 @@
+extends PhysicalUnit
+
+
+class_name PhysicalQuantityUnit
+
+
+export(float) var quantity = 1.0 setget set_quantity
+var physical_unit # TODO: remove after transitioning
+
+func _to_string():
+	return "[Quantity: [" + ._to_string() + ", " + str(quantity) + "]]"
+
+func set_quantity(value:float):
+	if value == null:
+		return
+	quantity = value
+	if numerical_unit == NumericalUnitSetting.DISCRETE:
+		quantity = floor(quantity)
+
+func add_quantity(value:float):
+	if value == null or value == 0.0:
+		return
+	set_quantity(quantity + value)
+
+func add_physical_quantity(value:PhysicalQuantityUnit):
+	if value == null:
+		return
+	if value.get_group_name() != get_group_name():
+		print("Error: Adding incompatible quantities ", str(value), " and ", str(self))
+		return
+	add_quantity(value.quantity)	
+
+func get_mass():
+	return quantity * mass
+
+func get_unit_mass():
+	return mass
+
+func get_unit_area():
+	return .get_area()
+
+func get_area():
+	return quantity * .get_area()
+
+#TODO: Remove redundant getters
+func get_group_name():
+	return group_name
+
+func get_readable_name():
+	return readable_name
+
+func get_world_space_scene():
+	print("FIX QuantityUnit: get_world_space_scene")
+	return
+
+func get_component_scene():
+	print("FIX QuantityUnit: get_component_scene")
+	return
+
+func get_quantity_for_area(value:float):
+	var unit_area = .get_area()
+	return value / unit_area
