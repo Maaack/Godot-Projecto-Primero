59e0e058643d4d89da8f14ee58405453076c4600
Maaack <marek.belski@gmail.com>
2020-04-01 21:01:39 -0700

    GAME-08: Trying to get info boxes to hover over nodes that don't have position data.

--- Objects/Interface/Button/Button.gd
+++ Objects/Interface/Button/Button.gd
@@ -5,10 +5,27 @@ onready var icon_node = $Icon
 
 export(Resource) var physical_unit setget set_physical_unit
 
+signal attention_on
+signal attention_off
+
+func _ready():
+	update_icon()
 
 func set_physical_unit(value:PhysicalUnit):
 	if value == null:
 		return
 	physical_unit = value
-	icon_node.physical_unit = value
+	update_icon()
+
+func update_icon():
+	if physical_unit == null or icon_node == null:
+		return
+	icon_node.physical_unit = physical_unit
 	icon_node.show()
+
+func _on_Button_mouse_entered():
+	emit_signal("attention_on", self)
+
+func _on_Button_mouse_exited():
+	if not pressed:
+		emit_signal("attention_off", self)
--- Objects/Interface/Inventory/Inventory.gd
+++ Objects/Interface/Inventory/Inventory.gd
@@ -6,9 +6,14 @@ const EMPTY_GROUP_NAME = 'EMPTY'
 onready var grid_node = $GridContainer
 onready var empty_value_node = $Header/EmptyValue
 
+export(Vector2) var default_info_box_offset = Vector2(0.0, 60.0)
+
 var button_scene = preload("res://Objects/Interface/Button/Button.tscn")
+var info_box_scene = preload("res://Objects/Interface/Window/RocketInspector/InfoBox/InfoBox.tscn")
 var physical_collection setget set_physical_collection
 var empty_quantity setget set_empty_quantity
+
+var button_info_box_map = {}
 
 func set_physical_collection(value:PhysicalCollection):
 	if value == null:
@@ -31,9 +36,34 @@ func update_inventory():
 			set_empty_quantity(physical_quantity)
 			continue
 		var button_instance = button_scene.instance()
-		grid_node.add_child(button_instance)
 		button_instance.physical_unit = physical_quantity
-		
+		grid_node.add_child(button_instance)
+		button_instance.connect("attention_on", self, "attach_info_box")
+
+func attach_info_box(value:Node):
+	if button_info_box_map.has(value):
+		return button_info_box_map[value]
+	var info_box_instance = info_box_scene.instance()
+	value.connect("attention_off", self, "detach_info_box")
+	add_child(info_box_instance)
+	button_info_box_map[value] = info_box_instance
+	info_box_instance.show()
+	info_box_instance.physical_unit = value.physical_unit
+	var info_box_position = get_local_mouse_position()
+	info_box_instance.position = info_box_position + default_info_box_offset
+	return info_box_instance
+
+func detach_info_box(value:Node):
+	if button_info_box_map.has(value):
+		var info_box_node = button_info_box_map[value]
+		if not is_instance_valid(info_box_node):
+			return
+		info_box_node.queue_free()
+		button_info_box_map.erase(value)
+
 func clear_inventory():
 	for child in grid_node.get_children():
 		child.queue_free()
+
+func _on_Button_positioned(value):
+	attach_info_box(value)
